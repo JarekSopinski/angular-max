f1ab122112cfedcd29f1da32684765462e65e6d9
JarekSopinski <jarek.sopinski@gmail.com>
2021-02-02 07:17:24 +0100

    Basics - exercise - practicing databinding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,9 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
 
-  allowNewServer:boolean = false;
+  allowNewServer = false;
   serverCreationStatus = 'No server was created!';
   serverName = 'Testserver';
+  userName = '';
 
   constructor() {
     setTimeout(() => { this.allowNewServer = true }, 2000);
@@ -19,11 +20,15 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created!';
+    this.serverCreationStatus = 'Server was created! Name is' + this.serverName;
   }
 
   onUpdateServerName(event: Event) {
     this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+  onRemoveUserName() {
+    this.userName = '';
   }
 
 }
