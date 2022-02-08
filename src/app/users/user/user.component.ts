ee613ccfc0b803b11b7b006441d323a86ce3972c
JarekSopinski <jarek.sopinski@gmail.com>
2022-02-08 18:51:05 +0100

    Fetching Updated Route Params with Observable

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -16,8 +16,15 @@ export class UserComponent implements OnInit {
   ngOnInit() {
     this.user = {
       id: this.route.snapshot.params['id'], // 'id' as definied in routes array in app module
-      name: this.route.snapshot.params['name']
+      name: this.route.snapshot.params['name'] // as above
     };
+    this.route.params // params is an observable, opposing to snapshot
+      .subscribe(
+        (params: Params) => { // 'params' here are UPDATED params
+          this.user.id = params['id'];
+          this.user.name = params['name'];
+        }
+      );
   }
 
 }
