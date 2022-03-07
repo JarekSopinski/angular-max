50e6c54113516f19362fb70f37cbcf89a35b12b7
JarekSopinski <jarek.sopinski@gmail.com>
2022-03-07 07:26:21 +0100

    Observable Error Example

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -21,13 +21,17 @@ export class HomeComponent implements OnInit, OnDestroy {
       let count = 0;
       setInterval(() => {
         observer.next(count);
+        if (count > 3) {
+          observer.error(new Error('Count in greater 3!') );
+        }
         count++;
       }, 1000);
     });
 
-    this.firstObsSubscription = customIntervalObservable.subscribe(data => {
-      console.log(data);
-    });
+    this.firstObsSubscription = customIntervalObservable.subscribe(
+      data => { console.log(data); },
+      error => { console.log(error); alert(error.message); }
+    );
 
   }
 
