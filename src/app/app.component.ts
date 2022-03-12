89c68c0ced17e0b897274aba770bd74aaf67d254
JarekSopinski <jarek.sopinski@gmail.com>
2022-03-12 06:01:40 +0100

    TD Form Exercise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,56 +7,17 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  @ViewChild('f') signupForm: NgForm;
-  defaultQuestion = 'pet';
-  answer = '';
-  genders = ['male', 'female'];
-  user = {
-    username: '',
-    email: '',
-    secretQuestion: '',
-    answer: '',
-    gender: ''
-  }
-  submitted = false;
+  @ViewChild('form', { static: false }) signupForm: NgForm;
 
-  suggestUserName() {
-    const suggestedName = 'Superuser';
-
-    // Approach 1 - overwrite whole form
-    // this.signupForm.setValue({
-    //   userData : {
-    //     username: suggestedName,
-    //     email: ''
-    //   },
-    //   secret: 'pet',
-    //   questionAnswer: '',
-    //   gender: 'male'
-    // });
-
-    // Approach 2 - only overwrite specific value
-    this.signupForm.form.patchValue({
-      userData: {
-        username: suggestedName
-      }
-    });
-
-  }
-
-  // onSubmit(form: NgForm) {
-  //   console.log(form);
-  // }
+  subscriptionOptions = [
+    { value: 'basic', label: 'Basic' },
+    { value: 'advanced', label: 'Advanced' },
+    { value: 'pro', label: 'Pro' }
+  ];
+  defaultSubscription = 'advanced';
 
   onSubmit() {
-    this.submitted = true;
-    console.log(this.signupForm);
-    this.user.username = this.signupForm.value.userData.username;
-    this.user.email = this.signupForm.value.userData.email;
-    this.user.secretQuestion = this.signupForm.value.secret;
-    this.user.answer = this.signupForm.value.questionAnswer;
-    this.user.gender = this.signupForm.value.gender;
-
-    this.signupForm.reset();
+    console.log(this.signupForm.value);
   }
 
 }
