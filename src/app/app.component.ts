5ac7e11ebae2c1652c9138d29f1092ed7624d573
JarekSopinski <jarek.sopinski@gmail.com>
2022-04-09 06:00:38 +0200

    Showing a Loading Indicator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ import { Post } from './post.model';
 })
 export class AppComponent implements OnInit {
   loadedPosts: Post[] = [];
+  isFetching: boolean = false;
 
   constructor(private http: HttpClient) {}
 
@@ -37,6 +38,7 @@ export class AppComponent implements OnInit {
   }
 
   private fetchPosts() {
+    this.isFetching = true;
     this.http.get<{ [key: string]: Post }>(
       'https://angular-max-334a3-default-rtdb.firebaseio.com/posts.json'
       )
@@ -51,6 +53,7 @@ export class AppComponent implements OnInit {
         })
       )
       .subscribe(posts => {
+        this.isFetching = false;
         this.loadedPosts = posts;
       });
   }
