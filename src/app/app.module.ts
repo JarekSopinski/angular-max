b8eb3f3ab6b535e8a49550903a3f9b7024d5e79c
JarekSopinski <jarek.sopinski@gmail.com>
2022-04-09 05:15:18 +0200

    Http section start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { ShortenPipe } from './shorten.pipe';
-import { FilterPipe } from './filter.pipe';
-import { ReverseStringPipe } from './reverse-string.pipe';
-import { SortbyPipe } from './sortby.pipe';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ShortenPipe,
-    FilterPipe,
-    ReverseStringPipe,
-    SortbyPipe
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
